67bc8edb7bfa7955e9e639fe51f091c2cdd103e5
NikhilAshodariya <nikhilashodariya123@gmail.com>
2018-01-31 22:36:01 +0530

    Removing unnecessary comments

--- server/model/BlackPawn.js
+++ server/model/BlackPawn.js
@@ -78,64 +78,6 @@ class BlackPawn extends Pawn {
       console.log("invalid Black Pawn Move");
       return "invalid";
     }
-
-
-    /*
-    if (((Number(from[0]) - 2) == to[0]) && (from[1] == to[1])) {
-      if (this.isFirstMove == true) {
-        if (ChessBoard.getPiece("" + (Number(from[0]) - 1) + from[1]) == undefined) {
-          if (ChessBoard.getPiece(to) != undefined) {
-            return "invalid";
-            // if (ChessBoard.getPiece(from).getType() == ChessBoard.getPiece(to).getType()) {
-            //   console.log("same party elements can't be killed");
-            //   return "invalid";
-            // }
-            // console.log("enemy Killed");
-            // console.log("Move successful");
-            // this.isFirstMove = false;
-            // var kp = ChessBoard.getPiece(to);
-            // KilledPieces[to] = kp;
-            // ChessBoard.setPiece(to, ChessBoard.getPiece(from));
-            // ChessBoard.setPiece(from, undefined);
-            // return "valid";
-          } else {
-            ChessBoard.setPiece(to, ChessBoard.getPiece(from));
-            ChessBoard.setPiece(from, undefined);
-            console.log("Move successful");
-            this.isFirstMove = false;
-            return "valid";
-          }
-        } else {
-          console.log("Invalid Move since someone in between");
-          return "invalid";
-        }
-      } else {
-        console.log("invalid Move since only first move can take 2 steps");
-        return "invalid";
-      }
-    } else if (((Number(from[0]) - 1) == to[0]) && (from[1]) == to[1]) {
-      if (ChessBoard.getPiece(to) != undefined) {
-        return "invalid";
-        console.log("enemy Killed");
-        console.log("Move successful");
-        this.isFirstMove = false;
-        var kp = ChessBoard.getPiece(to);
-        KilledPieces[to] = kp;
-        ChessBoard.setPiece(to, ChessBoard.getPiece(from));
-        ChessBoard.setPiece(from, undefined);
-        return "valid";
-      } else {
-        ChessBoard.setPiece(to, ChessBoard.getPiece(from));
-        ChessBoard.setPiece(from, undefined);
-        console.log("Move successful");
-        this.isFirstMove = false;
-        return "valid";
-      }
-    } else {
-      console.log("Move Unsuccessful learn pawn move direction");
-      return "invalid";
-    }
-    */
   }
 }
 
